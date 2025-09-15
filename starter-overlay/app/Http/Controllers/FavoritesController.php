26901f297439b43229856b802a661f74dcbf1ca0
Georgi Hristov <139592118+ghristov-prioritywire@users.noreply.github.com>
2025-09-15 02:20:16 -0500

    Add TODOs to FavoritesController methods

--- starter-overlay/app/Http/Controllers/FavoritesController.php
+++ starter-overlay/app/Http/Controllers/FavoritesController.php
@@ -4,31 +4,33 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\FavoriteStoreRequest;
 use App\Models\Quote;
 use App\Services\FavoritesService;
+use Illuminate\Http\Request;
 
+/**
+ * FavoritesController (TEMPLATE - Web)
+ * Call FavoritesService; keep pages simple and behavior as per spec.
+ */
 final class FavoritesController extends Controller
 {
     public function __construct(private FavoritesService $service) {}
 
     public function index()
     {
-        $favorites = $this->service->list(auth()->user());
-        return view('favorites', compact('favorites'));
+        // TODO: use $this->service->list(auth()->user())
+        throw new \LogicException('Not implemented: FavoritesController::index');
     }
 
-    public function store(FavoriteStoreRequest $request)
+    public function store(Request $request)
     {
-        $user = auth()->user();
-        $this->service->add($user, $request->string('text'), $request->input('author'));
-
-        return redirect()->back()->with('status', 'Added to favorites');
+        // TODO: validate text/author; $this->service->add(...); redirect back with status
+        throw new \LogicException('Not implemented: FavoritesController::store');
     }
 
     public function destroy(Quote $quote)
     {
-        $this->service->removeByQuoteId(auth()->user(), $quote->id);
-        return redirect()->back()->with('status', 'Removed from favorites');
+        // TODO: $this->service->removeByQuoteId(...); redirect back with status
+        throw new \LogicException('Not implemented: FavoritesController::destroy');
     }
 }
