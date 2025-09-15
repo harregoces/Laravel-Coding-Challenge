8faf350482a2488abcbf419a8f86764bf5e5021f
Georgi Hristov <139592118+ghristov-prioritywire@users.noreply.github.com>
2025-09-15 06:16:02 -0500

    Add login route with response

--- starter-overlay/routes/web.php
+++ starter-overlay/routes/web.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\FavoritesController;
 Route::get('/', fn() => redirect('/today'));
 Route::get('/today', TodayController::class);
 Route::get('/quotes', [QuotesController::class, 'index']);
+Route::get('/login', fn () => response('Login', 200))->name('login');
 
 Route::middleware('auth')->group(function () {
     Route::get('/favorites', [FavoritesController::class, 'index']);
